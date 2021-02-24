c190cc4cdf31ddf3ae398715f5dbdfe492a0ca79
Thibault Valton <t.valton@gmail.com>
2021-02-24 19:15:04 +0100

    init

---
+++ doc_scrapper.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import re
+
+file = open('ddhc.txt', 'r')
+stop_words_file= open('stop_words_french.txt', 'r')
+
+stop_words = ' '.join(line for line in stop_words_file.read().splitlines()).lower()
+content = ' '.join(line for line in file.read().splitlines()).lower().replace("'"," ").replace(","," ")
+content= re.sub("[0-9.,°€—'%)-]",'', content)
+#content= re.sub("[]", '', content)
+content = content.split(' ')
+
+freqs = {}
+for word in content:
+    if word in stop_words:
+        pass
+    else:
+        if word not in freqs:
+            freqs[word] = 1
+        else:
+            freqs[word] += 1
+file.close()
+
+
---
+++ main.py
@@ -0,0 +1,54 @@
+import dash
+import dash.dependencies as dd
+import dash_core_components as dcc
+import dash_html_components as html
+from doc_scrapper import freqs
+import numpy as np
+from PIL import Image
+from io import BytesIO
+
+from wordcloud import WordCloud, ImageColorGenerator
+import base64
+
+
+mask = np.array(Image.open("bonnet.jpg"))
+image_colors = ImageColorGenerator(mask)
+
+# external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__) #, external_stylesheets=external_stylesheets)
+
+
+app.layout = html.Div([
+    html.Img(id="image_wc"),
+])
+
+#/usr/share/fonts/truetype/msttcorefonts/times.ttf
+#/home/t/.local/share/fonts/FeFCrm27C.otf"
+#/home/t/Téléchargements/oldnewspapertypes/OldNewspaperTypes.ttf
+
+def plot_wordcloud(data):
+    d = data
+    wc = WordCloud(background_color='white',
+                   font_path="/usr/share/fonts/truetype/msttcorefonts/times.ttf",
+                   max_words = 2000,
+                   mask=mask,
+                   width=1024,
+                   height=1024,
+                   max_font_size=60,
+                   min_font_size=5,
+                   mode='RGBA'
+                   )
+    wc.fit_words(d)
+    wc.recolor(color_func=image_colors)
+
+    return wc.to_image()
+
+@app.callback(dd.Output('image_wc', 'src'), [dd.Input('image_wc', 'id')])
+def make_image(b):
+    img = BytesIO()
+    plot_wordcloud(data=freqs).save(img, format='PNG')
+    return 'data:image/png;base64,{}'.format(base64.b64encode(img.getvalue()).decode())
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
